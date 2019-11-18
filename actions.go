487c97c6029b62fbcf302ab6008d5c6c83fc90de
mikio nozaki <mikio.nozaki@gmail.com>
2019-11-19 00:26:40 +0900

    inital commit

---
+++ actions.go
@@ -0,0 +1,12 @@
+package actions
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func Demo() {
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	fmt.Printf("GOOS: %s\n", runtime.GOOS)
+	fmt.Printf("GOARCH: %s\n", runtime.GOARCH)
+}
